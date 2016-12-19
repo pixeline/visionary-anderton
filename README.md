f83224145543802bb14be3e9e88b9de9c7014f1d
Plennevaux Alexandre <aplennevaux@gmail.com>
2016-12-19 23:55:42 +0100

    gulp tasks set up

--- README.md
+++ README.md
@@ -7,9 +7,15 @@ L'extension chrome
 To Install Gulp, in your Terminal type:
 
 1. `npm install --global gulp-cli`
-2. `npm install gulp gulp-minify-css gulp-uglify gulp-clean gulp-cleanhtml gulp-jshint gulp-strip-debug gulp-zip --save-dev`
+2. `npm install gulp gulp-minify-css gulp-uglify gulp-clean gulp-cleanhtml jshint gulp-jshint gulp-strip-debug gulp-zip --save-dev`
+3. `npm update` to install remaining packages.
 
 ## starting your work...
 
-`gulp`
+`gulp` : This tasks cleans up the build folder then compiles js/css files, then serves the `build/`folder via URL: [http://localhost:2017](http://localhost:2017). It watches any changes you make on your js and scss folders, and live reload (via browserSync) your browser to reflect thoses changes. It is perfect for quick development.  In the background, it compiles any changes to the `./build` folder.
+
+## publishing the extension on the Play Store
+
+`gulp release` : this task creates the zip file in the `dist/`folder, that you can submit to the Play Store.
+
 
--- gulpfile.js
+++ gulpfile.js
@@ -8,40 +8,46 @@ var gulp = require('gulp'),
 	stripdebug = require('gulp-strip-debug'),
 	uglify = require('gulp-uglify'),
 	zip = require('gulp-zip');
+var sass = require('gulp-sass');
+var browsersync = require('browser-sync').create();
+const runSequence = require('run-sequence');
 //clean build directory
 gulp.task('clean', function() {
-	return gulp.src('build/*', {
+	return gulp.src('./build/*', {
 		read: false
 	}).pipe(clean());
 });
 //copy static folders to build directory
 gulp.task('copy', function() {
-	gulp.src('src/assets/fonts/**').pipe(gulp.dest('build/assets/fonts'));
-	gulp.src('src/assets/icons/**').pipe(gulp.dest('build/assets/icons'));
-	gulp.src('src/_locales/**').pipe(gulp.dest('build/_locales'));
-	return gulp.src('src/manifest.json').pipe(gulp.dest('build'));
+	gulp.src('./src/assets/fonts/**').pipe(gulp.dest('./build/assets/fonts'));
+	gulp.src('./src/icons/**').pipe(gulp.dest('./build/icons'));
+	gulp.src('./src/_locales/**').pipe(gulp.dest('./build/_locales'));
+	return gulp.src('./src/manifest.json').pipe(gulp.dest('./build'));
 });
 //copy and compress HTML files
 gulp.task('html', function() {
-	return gulp.src('src/*.html').pipe(cleanhtml()).pipe(gulp.dest('build'));
+	return gulp.src('./src/*.html').pipe(cleanhtml()).pipe(gulp.dest('./build'));
 });
 //run scripts through JSHint
 gulp.task('jshint', function() {
-	return gulp.src('src/assets/js/*.js').pipe(jshint()).pipe(jshint.reporter('default'));
+	return gulp.src('./src/assets/js/*.js').pipe(jshint()).pipe(jshint.reporter('default'));
 });
 //copy vendor scripts and uglify all other scripts, creating source maps
 gulp.task('scripts', ['jshint'], function() {
-	gulp.src('src/assets/js/vendors/**/*.js').pipe(gulp.dest('build/assets/js/vendors'));
-	return gulp.src(['src/assets/js/**/*.js', '!src/assets/js/vendors/**/*.js']).pipe(stripdebug()).pipe(uglify({
+	gulp.src('./src/assets/js/vendors/**/*.js').pipe(gulp.dest('./build/assets/js/vendors'));
+	return gulp.src(['./src/assets/js/**/*.js', '!src/assets/js/vendors/**/*.js']).pipe(stripdebug()).pipe(uglify({
 		outSourceMap: true
-	})).pipe(gulp.dest('build/assets/js'));
+	})).pipe(gulp.dest('./build/assets/js'));
 });
 //minify styles
-gulp.task('styles', function() {
-	// 	return gulp.src('src/styles/**/*.css')
-	// 		.pipe(minifycss({root: 'src/styles', keepSpecialComments: 0}))
-	// 		.pipe(gulp.dest('build/styles'));
-	return gulp.src('src/assets/css/**').pipe(gulp.dest('build/assets/css'));
+gulp.task('styles', ['sass'], function() {
+	return gulp.src('./src/assets/css/**').pipe(minifycss({
+		root: './src/assets/css',
+		keepSpecialComments: 0
+	})).pipe(gulp.dest('./build/assets/css'));
+});
+gulp.task('sass', function() {
+	return gulp.src('./src/assets/scss/*.scss').pipe(sass().on('error', sass.logError)).pipe(gulp.dest('./src/assets/css'));
 });
 //build ditributable and sourcemaps after other tasks completed
 gulp.task('zip', ['html', 'scripts', 'styles', 'copy'], function() {
@@ -49,11 +55,46 @@ gulp.task('zip', ['html', 'scripts', 'styles', 'copy'], function() {
 		distFileName = manifest.name + ' v' + manifest.version + '.zip',
 		mapFileName = manifest.name + ' v' + manifest.version + '-maps.zip';
 	//collect all source maps
-	gulp.src('build/assets/js/**/*.map').pipe(zip(mapFileName)).pipe(gulp.dest('dist'));
+	gulp.src('./build/assets/js/**/*.map').pipe(zip(mapFileName)).pipe(gulp.dest('./dist'));
 	//build distributable extension
-	return gulp.src(['build/**', '!build/assets/js/**/*.map']).pipe(zip(distFileName)).pipe(gulp.dest('dist'));
+	return gulp.src(['./build/**', '!build/assets/js/**/*.map']).pipe(zip(distFileName)).pipe(gulp.dest('./dist'));
 });
 //run all tasks after build directory has been cleaned
-gulp.task('default', ['clean'], function() {
+gulp.task('release', ['clean'], function() {
 	gulp.start('zip');
+});
+// Browsersync tasks
+gulp.task('browser-sync', function() {
+	browsersync.init({
+		server: {
+			baseDir: ['./build']
+		},
+		files: ['./build/**/*'],
+		notify: false,
+		port: 2017
+	});
+});
+gulp.task('browsersync-reload', function() {
+	browsersync.reload({
+		stream: true
+	});
+});
+gulp.task('watch', ['browser-sync'], function() {
+	gulp.watch(['./src/*.html'], ['html']);
+	gulp.watch(['./src/assets/scss/**/*.scss'], ['styles']);
+	gulp.watch(['./src/assets/js/**/*.js'], ['scripts', 'browsersync-reload']);
+});
+gulp.task('default', ['build','watch']);
+gulp.task('build', function() {	
+	runSequence(
+	'clean',
+	'copy', 
+	'html', 
+	'styles', 
+	'scripts',
+	function(error) {
+		if (error) {
+			console.log(error.message);
+		}
+	});
 });
