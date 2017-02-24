1030f7287d71656742018867fc2cbd2588cf4c68
Plennevaux Alexandre <aplennevaux@gmail.com>
2017-02-24 17:53:10 +0100

    (wip)
    Unstable result of my debug session:
    Everything is usable EXCEPT app.js which is cleaner, but not working
    yet.

--- gulpfile.js
+++ gulpfile.js
@@ -53,15 +53,17 @@ gulp.task('jshint', function() {
 });
 
 //copy vendor scripts and uglify all other scripts, creating source maps
-gulp.task('scripts', ['jshint'], function() {
+gulp.task('scripts', function() {
 	gulp.src(project.src_dir + '/assets/js/vendors/**/*.js')
 		.pipe(gulp.dest(project.build_dir + '/assets/js/vendors'));
 	
 	return gulp.src([project.src_dir + '/assets/js/*.js'])
+/*
 		.pipe(stripdebug())
 		.pipe(uglify({
 			outSourceMap: true
 		}))
+*/
 		.pipe(gulp.dest(project.build_dir + '/assets/js'));
 });
 
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,54 +1,108 @@
-(function($) {
+var visionary = {
+	api: 'https://dev.colour-blindness.org/api'
+};
+// BEGIN
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+	chrome.extension.getBackgroundPage().updateTabs();
+});
 
-	// TURN ON/OFF Color Correction
-	// Pour le rendre actif, Voir documentation: http://semantic-ui.com/modules/checkbox.html#/definition
+checkTwitterLogin();
+
+// TURN ON/OFF Color Correction
+// Pour le rendre actif, Voir documentation: http://semantic-ui.com/modules/checkbox.html#/definition
+$('#js-status-indicator').checkbox({
+
+	onChecked: function() {
+		$('#js-status-indicator-label').text('activée');
+		$("#severity-slider-div").removeClass("hide");
+		$("#js-delta-slider").removeClass("hide");
+
+	},
+	onUnchecked: function() {
+		$('#js-status-indicator-label').text('désactivée');
+		$("#severity-slider-div").addClass("hide");
+		$("#js-delta-slider").addClass("hide");
+	}
+}).checkbox('uncheck'); // utilise "uncheck" pour la mettre en mode "désactivé".
+// $('#js-current-page-title').text(PAGETITLE)  <--- doit recevoir le contenu de la balise TITLE de la page courante.
+// Slider that sets intensity
+$('#js-delta-slider').range({
+	min: -1,
+	max: 1,
+	start: -0.5,
+	step: 0.1,
+	onChange: function(value) {
+		chrome.extension.getBackgroundPage().setDelta(value);
+	}
+});
+
+$('#js-severity-slider').range({
+	min: -0.5,
+	max: 0.5,
+	start: -0.1,
+	step: 0.1,
+	onChange: function(value) {
+		//console.log('severity = ' + value);
+		chrome.extension.getBackgroundPage().setSeverity(value);
+	}
+});
 
 
-	$('#js-status-indicator').checkbox({
+$("#signin").on('click', function() {
 
-		onChecked: function() {
-			$('#js-status-indicator-label').text('activée');
-			$("#severity-slider-div").removeClass("hide");
-			$("#js-delta-slider").removeClass("hide");
+	var payload = {
+		login: "none",
+		pwd: "none"
+	};
+	var username = $("#email").val();
+	var userpass = $("#mdp").val();
+	payload.login = username;
+	payload.pwd = userpass;
 
-		},
-		onUnchecked: function() {
-			$('#js-status-indicator-label').text('désactivée');
-			$("#severity-slider-div").addClass("hide");
-			$("#js-delta-slider").addClass("hide");
-		}
-	}).checkbox('uncheck'); // utilise "uncheck" pour la mettre en mode "désactivé".
+	console.log(payload);
 
-	// $('#js-current-page-title').text(PAGETITLE)  <--- doit recevoir le contenu de la balise TITLE de la page courante.
-
-	// Slider that sets intensity
-	$('#js-delta-slider').range({
-		min: -1,
-		max: 1,
-		start: -0.5,
-		step: 0.1,
-		onChange: function(value) {
-			chrome.extension.getBackgroundPage().setDelta(value);
-		}
+	var resultToken = getToken(payload);
+	console.log('Anderton_token=', localStorage.getItem('Anderton_token'));
+	var checkedBoxes = $('input[name=check]:checked');
+	console.log(checkedBoxes);
+	chrome.browserAction.setBadgeText({
+		text: "ON"
+	});
+	var diagratio = localStorage.getItem("Diag_ratio");
+	$("js-diagnostic-percentage").html(diagratio);
+	chrome.runtime.sendMessage(payload, function(response) {
+		console.log(response.farewell);
 	});
 
-	$('#js-severity-slider').range({
-		min: -0.5,
-		max: 0.5,
-		start: -0.1,
-		step: 0.1,
-		onChange: function(value) {
-			//console.log('severity = ' + value);
-			chrome.extension.getBackgroundPage().setSeverity(value);
-		}
-	});
 
-	// FIN
-})(jQuery);
-//alert ('Anderton is running');
+});
+$("#twitter").on("click", twitterLogin);
+
+
+// Google Login
+$("#google").on("click", function(e) {
+
+	e.preventDefault();
+	$('#js-sso-result').html("start...");
+	chrome.identity.getAuthToken({
+		'interactive': false
+	}, function(token) {
+		localStorage.setItem('google_access_token', token);
+		$('#js-sso-result').html("received: " + token);
+	});
+	$('#js-sso-result').html("finished..." + token);
+});
+
+
+$("#register-user").on("click", function() {
+	window.location.href = "subscribe.html";
+});
+$("#register-btn").on("click", register);
+$("#signout").on("click", signoff);
+// END.
 
 function checkTwitterLogin() {
-	var twitterToken = localStorage['oauth_token_secret_twitter'];
+	var twitterToken = localStorage.getItem('oauth_token_secret_twitter');
 	if (twitterToken) {
 		if (validateTwitterToken(twitterToken)) {
 			chrome.browserAction.setPopup({
@@ -58,12 +112,12 @@ function checkTwitterLogin() {
 			console.log('wrong twitter authentication');
 		}
 	}
-};
+}
 
 function validateTwitterToken(token) {
 	$.ajax({
 		type: "POST",
-		url: "https://dev.colour-blindness.org/api/oauth",
+		url: visionary.api + "/oauth",
 		data: {
 			"provider": "twitter",
 			"token": token,
@@ -85,19 +139,22 @@ function login() {
 		login: "none",
 		pwd: "none"
 	};
-	var username = document.getElementById("email").value;
-	var userpass = document.getElementById("mdp").value;
+	var username = $("#email").val();
+	var userpass = $("#mdp").val();
 	payload.login = username;
 	payload.pwd = userpass;
+
+	console.log(payload);
+
 	var resultToken = getToken(payload);
-	console.log('Anderton_token=', localStorage['Anderton_token']);
-	var checkedBoxes = document.querySelectorAll('input[name=check]:checked');
+	console.log('Anderton_token=', localStorage.getItem('Anderton_token'));
+	var checkedBoxes = $('input[name=check]:checked');
 	console.log(checkedBoxes);
 	chrome.browserAction.setBadgeText({
 		text: "ON"
 	});
-	var diagratio = localStorage["Diag_ratio"];
-	document.getElementById("js-diagnostic-percentage").innerHTML = diagratio;
+	var diagratio = localStorage.getItem("Diag_ratio");
+	$("js-diagnostic-percentage").html(diagratio);
 	chrome.runtime.sendMessage(payload, function(response) {
 		console.log(response.farewell);
 	});
@@ -135,7 +192,7 @@ function getToken(userObject) {
 	var psword = userObject.pwd;
 	$.ajax({
 		type: "POST",
-		url: "https://dev.colour-blindness.org/api/oauth",
+		url: visionary.api + "/oauth",
 		data: {
 			"email": usrname,
 			"password": psword,
@@ -150,7 +207,7 @@ function getToken(userObject) {
 				window.location.href = "nextindex.html";
 			} else if (data.token) {
 				console.log('IN Local Storage');
-				localStorage['Anderton_token'] = data.token;
+				localStorage.setItem('Anderton_token', data.token);
 				chrome.browserAction.setPopup({
 					//tabId: tab.id[0],			// Set the new popup for this tab.
 					popup: 'nextindex.html' // Open this html file within the popup.
@@ -167,26 +224,17 @@ function getToken(userObject) {
 
 function twitterLogin() {
 	chrome.extension.getBackgroundPage().authenticateTwitter();
-};
-
-function googleLogin() {
-	chrome.identity.getAuthToken({
-		'interactive': true
-	}, function(token) {
-		localStorage['google_access_token'] = token;
-	});
-};
-
-function switchToRegister() {
-	window.location.href = "subscribe.html";
 }
 
+
+
+
 function register() {
-	var username = document.getElementById("register-email").value;
-	var userpass = document.getElementById("register-password").value;
+	var username = $("#register-email").val();
+	var userpass = $("#register-password").val();
 	$.ajax({
 		type: "POST",
-		url: "https://dev.colour-blindness.org/api/subscribe",
+		url: visionary.api + "/subscribe",
 		data: {
 			"email": username,
 			"password": userpass
@@ -203,32 +251,3 @@ function register() {
 		dataType: "json"
 	});
 }
-
-// BEGIN
-document.addEventListener("DOMContentLoaded", function(event) {
-	if (document.getElementById("signin")) {
-		document.getElementById("signin").addEventListener("click", login);
-	}
-	if (document.getElementById("twitter")) {
-		document.getElementById("twitter").addEventListener("click", twitterLogin);
-	}
-	if (document.getElementById("signout")) {
-		document.getElementById("signout").addEventListener("click", signoff);
-	}
-	if (document.getElementById("google")) {
-		document.getElementById("google").addEventListener("click", googleLogin);
-	}
-	if (document.getElementById("register-user")) {
-		document.getElementById("register-user").addEventListener("click", switchToRegister);
-	}
-	if (document.getElementById("register-btn")) {
-		document.getElementById("register-btn").addEventListener("click", register);
-	}
-});
-
-chrome.storage.onChanged.addListener(function(changes, namespace) {
-	chrome.extension.getBackgroundPage().updateTabs();
-});
-
-checkTwitterLogin();
-// END.
